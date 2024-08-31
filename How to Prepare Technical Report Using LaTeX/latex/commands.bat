cade3eff251844bd09d592238aa2c18797a24cf0
Eldesouky Elkhouly <41583206+EldesoukyElkhouly@users.noreply.github.com>
2024-08-31 13:16:12 +0300

    Add files via upload

---
+++ How to Prepare Technical Report Using LaTeX/latex/commands.bat
@@ -0,0 +1,5 @@
+Pdflatex report.tex
+Pdflatex report.tex
+makeindex report.nlo -s nomencl.ist -o report.nls
+Pdflatex report.tex
+report.pdf
---
+++ How to Prepare Technical Report Using LaTeX/latex/program.java
@@ -0,0 +1,7 @@
+/* this is a simple Java program */
+class Example {
+    // your program begins with a call to main()
+    public static void main(String args[]){
+      System.out.println("this is a simple Java program");
+    }
+}
